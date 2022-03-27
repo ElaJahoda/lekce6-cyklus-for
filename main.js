8b3e88fcf0797d4a0f7b8d6b605644c23f54ac1c
Ela Jahoda <3la.jah0da@gmail.com>
2022-03-27 08:59:31 +0200

    cykly s pouzitim while

--- main.js
+++ main.js
@@ -4,28 +4,29 @@
 
 // První příklad - vypiš vzestupně čísla od 0 do 10
 console.log('První příklad - vzestupná řada 0 - 10');
-
 //TADY NAPIS CYKLUS
-
-console.log('-------------------');
-
-
+let i = 0;
+while(i<=10) {
+    console.log(i);
+    i++;
+}
 
 // Druhý příklad - zkopíruj předchozí příklad
 // a vyzkoušej, jaký je rozdíl mezi <= a <
 // v podmínce uvnitř cyklu
-
 console.log('Druhý příklad - rozdíl mezi <= a <');
-
 //TADY NAPIS CYKLUS
-
-console.log('-------------------');
-
-
+i = 0;
+while(i<11) {
+    console.log(i);
+    i++;
+}
 
 // Třetí příklad - vypiš sestupně čísla od 10 do 0
 console.log('Třetí příklad - sestupná řada 10 - 0');
-
 //TADY NAPIS CYKLUS
-
-console.log('-------------------');
+i = 10;
+while(i>=0) {
+    console.log(i);
+    i--;
+}
